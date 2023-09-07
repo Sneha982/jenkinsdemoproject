abc67c957b2f047c003c5718e68b68e114b5ac23
Sneha982 <gunda.sneha98@gmail.com>
2023-09-07 20:30:52 +0530

    added my project to repo

---
+++ demojenkinsproject/src/main/java/com/sneha/demojenkinsproject/DemojenkinsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sneha.demojenkinsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojenkinsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojenkinsprojectApplication.class, args);
+	}
+
+}
